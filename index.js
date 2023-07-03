19260beca6a78497f648e262b780ca5ba7e5e708
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-07-03 18:46:01 -0300

    readFile sync e async

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-import chalk from 'chalk';
+import fs from "fs";
+import chalk from "chalk";
 
-console.log(chalk.blue('Hello, world!'));
+// Assincrono async
+async function getFile(path) {
+  try {
+    const content = await fs.promises.readFile(path, "utf-8");
+    console.log(chalk.green(content));
+  } catch (error) {
+    throw new Error(chalk.red(error));
+  }
+}
+
+// Assincrono then
+// function getFile(path) {
+//   fs.promises
+//     .readFile(path, "utf-8")
+//     .then((content) => console.log(chalk.green(content)))
+//     .catch((error) => {
+//       throw new Error(chalk.red(error));
+//     });
+// }
+
+// Sincrono
+// function getFile(path) {
+//   fs.readFile(path, "utf-8", (error, content) => {
+//     if (error) {
+//       throw new Error(chalk.red(error));
+//     }
+//     console.log(chalk.green(content));
+//   });
+// }
+
+getFile("./files/text.md");
